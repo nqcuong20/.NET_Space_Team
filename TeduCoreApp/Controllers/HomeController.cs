5f17f126f7c674a4404a90e82efc02d343aef93b
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-11-13 23:03:06 +0700

    validation & claim

--- TeduCoreApp/Controllers/HomeController.cs
+++ TeduCoreApp/Controllers/HomeController.cs
@@ -5,13 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TeduCoreApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using TeduCoreApp.Extensions;
 
 namespace TeduCoreApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            var email = User.GetSpecificClaim("Email");
             return View();
         }
 
--- TeduCoreApp/wwwroot/lib/jquery-sparkline/src/chart-pie.js
+++ TeduCoreApp/wwwroot/lib/jquery-sparkline/src/chart-pie.js
@@ -90,7 +90,6 @@
                 options = this.options,
                 radius = this.radius,
                 borderWidth = options.get('borderWidth'),
-                donutWidth = options.get('donutWidth'),
                 shape, i;
 
             if (!pie._super.render.call(this)) {
@@ -106,10 +105,6 @@
                     this.valueShapes[i] = shape.id; // store just the shapeid
                     this.shapes[shape.id] = i;
                 }
-            }
-            if (donutWidth) {
-                target.drawCircle(radius, radius, radius - donutWidth, options.get('donutColor'), 
-                    options.get('donutColor'), 0).append();
             }
             target.render();
         }

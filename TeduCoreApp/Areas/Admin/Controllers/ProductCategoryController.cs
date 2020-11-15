174fbdcf154f1e71ded073d40921ee4bb50b6cbb
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-11-15 23:40:55 +0700

    create tree product-category use jeasyui

---
+++ TeduCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TeduCoreApp.Application.Interfaces;
+
+namespace TeduCoreApp.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+     
+        IProductCategoryService _productCategoryService;
+        public ProductCategoryController( IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region GetData API
+        [HttpGet]
+        public  IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+        #endregion
+    }
+}
---
+++ TeduCoreApp/wwwroot/app/Controllers/product-category/index.js
@@ -0,0 +1,33 @@
+﻿var productCategoryController = function () {
+    this.initialize = function () {
+        loadData();
+    }
+    //LOAD DỮ LIỆU RA
+    function loadData() {
+        $.ajax({
+            url: '/Admin/ProductCategory/GetAll',//lấy data
+            dataType: 'json', //kiểu data
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+
+                });
+                var treeArr = tedu.unflattern(data);
+
+                //var $tree = $('#treeProductCategory');
+
+                $('#treeProductCategory').tree({
+                    data: treeArr,
+                    dnd: true
+                });
+
+            }
+        });
+    }
+}

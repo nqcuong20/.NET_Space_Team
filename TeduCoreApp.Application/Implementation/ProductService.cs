71be33628c3c762cc29497d5994cdc082d4ce1a3
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-11-15 13:17:48 +0700

    debug & phan trang product

--- TeduCoreApp.Application/Implementation/ProductService.cs
+++ TeduCoreApp.Application/Implementation/ProductService.cs
@@ -6,6 +6,7 @@ using System.Text;
 using TeduCoreApp.Application.Interfaces;
 using TeduCoreApp.Application.ViewModels.Product;
 using TeduCoreApp.Data.IRepositories;
+using TeduCoreApp.Utilities.Dtos;
 
 namespace TeduCoreApp.Application.Implementation
 {
@@ -24,6 +25,28 @@ namespace TeduCoreApp.Application.Implementation
         public List<ProductViewModel> GetAll()
         {
             return _productRepository.FindAll(x => x.ProductCategory).ProjectTo<ProductViewModel>().ToList();
+        }
+
+        public PagedResult<ProductViewModel> GetAllPaging(int? categoryId, string keyword, int page, int pageSize)
+        {
+            var query = _productRepository.FindAll(x => x.Status == Data.Enums.Status.Active);
+            if (!string.IsNullOrEmpty(keyword))
+                query = query.Where(x => x.Name.Contains(keyword));
+            if (categoryId.HasValue)
+                query = query.Where(x => x.CategoryId == categoryId.Value);
+            int totalRow = query.Count();
+            query = query.OrderByDescending(x => x.DateCreated)
+                .Skip((page - 1) * pageSize).Take(pageSize);
+            var data = query.ProjectTo<ProductViewModel>().ToList();
+
+            var paginationSet = new PagedResult<ProductViewModel>()
+            {
+                Results = data,
+                CurrentPage = page,
+                RowCount = totalRow,
+                PageSize = pageSize
+            };
+            return paginationSet;
         }
     }
 }
--- TeduCoreApp/Controllers/AccountController.cs
+++ TeduCoreApp/Controllers/AccountController.cs
@@ -17,7 +17,6 @@ using TeduCoreApp.Data.Entities;
 
 namespace TeduCoreApp.Controllers
 {
-    [Authorize]
     [Route("[controller]/[action]")]
     public class AccountController : Controller
     {

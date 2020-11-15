9373466850156bdc831c984fa5dfe77c47deb40f
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-11-15 23:50:27 +0700

    sort drag and drop use jeasyui

--- TeduCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
+++ TeduCoreApp/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -9,23 +9,69 @@ namespace TeduCoreApp.Areas.Admin.Controllers
 {
     public class ProductCategoryController : BaseController
     {
-     
         IProductCategoryService _productCategoryService;
-        public ProductCategoryController( IProductCategoryService productCategoryService)
+        public ProductCategoryController(IProductCategoryService productCategoryService)
         {
             _productCategoryService = productCategoryService;
         }
+
         public IActionResult Index()
         {
             return View();
         }
-        #region GetData API
+
+        #region Get Data API
         [HttpGet]
-        public  IActionResult GetAll()
+        public IActionResult GetAll()
         {
             var model = _productCategoryService.GetAll();
             return new OkObjectResult(model);
         }
+
+        [HttpPost]
+        public IActionResult UpdateParentId(int sourceId, int targetId, Dictionary<int, int> items)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                if (sourceId == targetId)
+                {
+                    return new BadRequestResult();
+                }
+                else
+                {
+                    _productCategoryService.UpdateParentId(sourceId, targetId, items);
+                    _productCategoryService.Save();
+                    return new OkResult();
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ReOrder(int sourceId, int targetId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                if (sourceId == targetId)
+                {
+                    return new BadRequestResult();
+                }
+                else
+                {
+                    _productCategoryService.ReOrder(sourceId, targetId);
+                    _productCategoryService.Save();
+                    return new OkResult();
+                }
+            }
+        }
+
         #endregion
     }
 }
--- TeduCoreApp/wwwroot/app/Controllers/product-category/index.js
+++ TeduCoreApp/wwwroot/app/Controllers/product-category/index.js
@@ -2,11 +2,11 @@
     this.initialize = function () {
         loadData();
     }
-    //LOAD DỮ LIỆU RA
+
     function loadData() {
         $.ajax({
-            url: '/Admin/ProductCategory/GetAll',//lấy data
-            dataType: 'json', //kiểu data
+            url: '/Admin/ProductCategory/GetAll',
+            dataType: 'json',
             success: function (response) {
                 var data = [];
                 $.each(response, function (i, item) {
@@ -19,12 +19,58 @@
 
                 });
                 var treeArr = tedu.unflattern(data);
-
+                treeArr.sort(function (a, b) {
+                    return a.sortOrder - b.sortOrder;
+                });
                 //var $tree = $('#treeProductCategory');
 
                 $('#treeProductCategory').tree({
                     data: treeArr,
-                    dnd: true
+                    dnd: true,
+                    onDrop: function (target, source, point) {
+                        console.log(target);
+                        console.log(source);
+                        console.log(point);
+                        var targetNode = $(this).tree('getNode', target);
+                        if (point === 'append') {
+                            var children = [];
+                            $.each(targetNode.children, function (i, item) {
+                                children.push({
+                                    key: item.id,
+                                    value: i
+                                });
+                            });
+
+                            //Update to database
+                            $.ajax({
+                                url: '/Admin/ProductCategory/UpdateParentId',
+                                type: 'post',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id,
+                                    items: children
+                                },
+                                success: function (res) {
+                                    loadData();
+                                }
+                            });
+                        }
+                        else if (point === 'top' || point === 'bottom') {
+                            $.ajax({
+                                url: '/Admin/ProductCategory/ReOrder',
+                                type: 'post',
+                                dataType: 'json',
+                                data: {
+                                    sourceId: source.id,
+                                    targetId: targetNode.id
+                                },
+                                success: function (res) {
+                                    loadData();
+                                }
+                            });
+                        }
+                    }
                 });
 
             }

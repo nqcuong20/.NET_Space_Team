9f323ecf77408ca3e128d810799b1326db258590
Nguyễn Quốc Cường <60700939+nqcuong20@users.noreply.github.com>
2020-11-16 03:06:33 +0700

    update delete product

--- TeduCoreApp.Application/Implementation/ProductService.cs
+++ TeduCoreApp.Application/Implementation/ProductService.cs
@@ -1,22 +1,80 @@
-﻿using AutoMapper.QueryableExtensions;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TeduCoreApp.Application.Interfaces;
 using TeduCoreApp.Application.ViewModels.Product;
+using TeduCoreApp.Data.Entities;
+using TeduCoreApp.Data.Enums;
 using TeduCoreApp.Data.IRepositories;
+using TeduCoreApp.Infrastructure.Interfaces;
+using TeduCoreApp.Utilities.Constants;
 using TeduCoreApp.Utilities.Dtos;
+using TeduCoreApp.Utilities.Helpers;
 
 namespace TeduCoreApp.Application.Implementation
 {
     public class ProductService : IProductService
     {
         IProductRepository _productRepository;
-        public ProductService(IProductRepository productRepository)
+        ITagRepository _tagRepository;
+        IProductTagRepository _productTagRepository;
+        IUnitOfWork _unitOfWork;
+        public ProductService(IProductRepository productRepository,
+            ITagRepository tagRepository,
+            IUnitOfWork unitOfWork,
+        IProductTagRepository productTagRepository)
         {
             _productRepository = productRepository;
+            _tagRepository = tagRepository;
+            _productTagRepository = productTagRepository;
+            _unitOfWork = unitOfWork;
         }
+
+        public ProductViewModel Add(ProductViewModel productVm)
+        {
+            List<ProductTag> productTags = new List<ProductTag>();
+            if (!string.IsNullOrEmpty(productVm.Tags))
+            {
+                string[] tags = productVm.Tags.Split(',');
+                foreach (string t in tags)
+                {
+                    var tagId = TextHelper.ToUnsignString(t);
+                    if (!_tagRepository.FindAll(x => x.Id == tagId).Any())
+                    {
+                        Tag tag = new Tag
+                        {
+                            Id = tagId,
+                            Name = t,
+                            Type = CommonConstants.ProductTag
+                        };
+                        _tagRepository.Add(tag);
+                    }
+
+                    ProductTag productTag = new ProductTag
+                    {
+                        TagId = tagId
+                    };
+                    productTags.Add(productTag);
+                }
+                var product = Mapper.Map<ProductViewModel, Product>(productVm);
+                foreach (var productTag in productTags)
+                {
+                    product.ProductTags.Add(productTag);
+                }
+                _productRepository.Add(product);
+
+            }
+            return productVm;
+        }
+
+        public void Delete(int id)
+        {
+            _productRepository.Remove(id);
+        }
+
         public void Dispose()
         {
             GC.SuppressFinalize(this);
@@ -29,14 +87,17 @@ namespace TeduCoreApp.Application.Implementation
 
         public PagedResult<ProductViewModel> GetAllPaging(int? categoryId, string keyword, int page, int pageSize)
         {
-            var query = _productRepository.FindAll(x => x.Status == Data.Enums.Status.Active);
+            var query = _productRepository.FindAll(x => x.Status == Status.Active);
             if (!string.IsNullOrEmpty(keyword))
                 query = query.Where(x => x.Name.Contains(keyword));
             if (categoryId.HasValue)
                 query = query.Where(x => x.CategoryId == categoryId.Value);
+
             int totalRow = query.Count();
+
             query = query.OrderByDescending(x => x.DateCreated)
                 .Skip((page - 1) * pageSize).Take(pageSize);
+
             var data = query.ProjectTo<ProductViewModel>().ToList();
 
             var paginationSet = new PagedResult<ProductViewModel>()
@@ -47,6 +108,51 @@ namespace TeduCoreApp.Application.Implementation
                 PageSize = pageSize
             };
             return paginationSet;
+        }
+
+        public ProductViewModel GetById(int id)
+        {
+            return Mapper.Map<Product, ProductViewModel>(_productRepository.FindById(id));
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(ProductViewModel productVm)
+        {
+            List<ProductTag> productTags = new List<ProductTag>();
+
+            if (!string.IsNullOrEmpty(productVm.Tags))
+            {
+                string[] tags = productVm.Tags.Split(',');
+                foreach (string t in tags)
+                {
+                    var tagId = TextHelper.ToUnsignString(t);
+                    if (!_tagRepository.FindAll(x => x.Id == tagId).Any())
+                    {
+                        Tag tag = new Tag();
+                        tag.Id = tagId;
+                        tag.Name = t;
+                        tag.Type = CommonConstants.ProductTag;
+                        _tagRepository.Add(tag);
+                    }
+                    _productTagRepository.RemoveMultiple(_productTagRepository.FindAll(x => x.Id == productVm.Id).ToList());
+                    ProductTag productTag = new ProductTag
+                    {
+                        TagId = tagId
+                    };
+                    productTags.Add(productTag);
+                }
+            }
+
+            var product = Mapper.Map<ProductViewModel, Product>(productVm);
+            foreach (var productTag in productTags)
+            {
+                product.ProductTags.Add(productTag);
+            }
+            _productRepository.Update(product);
         }
     }
 }
--- TeduCoreApp/Areas/Admin/Controllers/ProductController.cs
+++ TeduCoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using TeduCoreApp.Application.Interfaces;
+using TeduCoreApp.Application.ViewModels.Product;
+using TeduCoreApp.Utilities.Helpers;
 
 namespace TeduCoreApp.Areas.Admin.Controllers
 {
     public class ProductController : BaseController
     {
-        IProductService _productService;
-        IProductCategoryService _productCategoryService;
+        private IProductService _productService;
+        private IProductCategoryService _productCategoryService;
+
         public ProductController(IProductService productService, IProductCategoryService productCategoryService)
         {
             _productService = productService;
@@ -23,25 +27,76 @@ namespace TeduCoreApp.Areas.Admin.Controllers
         }
 
         #region AJAX API
+
         [HttpGet]
         public IActionResult GetAll()
         {
             var model = _productService.GetAll();
             return new OkObjectResult(model);
         }
+
         [HttpGet]
         public IActionResult GetAllCategories()
         {
             var model = _productCategoryService.GetAll();
             return new OkObjectResult(model);
         }
+
         [HttpGet]
-        public IActionResult GetAllPaging(int?  categoryId, string keyword, int page, int pageSize)
+        public IActionResult GetAllPaging(int? categoryId, string keyword, int page, int pageSize)
         {
-            var model = _productService.GetAllPaging( categoryId,  keyword,page, pageSize);
+            var model = _productService.GetAllPaging(categoryId, keyword, page, pageSize);
             return new OkObjectResult(model);
         }
-        
-        #endregion
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            var model = _productService.GetById(id);
+
+            return new OkObjectResult(model);
+        }
+
+        [HttpPost]
+        public IActionResult SaveEntity(ProductViewModel productVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return new BadRequestObjectResult(allErrors);
+            }
+            else
+            {
+                productVm.SeoAlias = TextHelper.ToUnsignString(productVm.Name);
+                if (productVm.Id == 0)
+                {
+                    _productService.Add(productVm);
+                }
+                else
+                {
+                    _productService.Update(productVm);
+                }
+                _productService.Save();
+                return new OkObjectResult(productVm);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                _productService.Delete(id);
+                _productService.Save();
+
+                return new OkObjectResult(id);
+            }
+        }
+
+        #endregion AJAX API
     }
 }
